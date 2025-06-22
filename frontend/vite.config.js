0bee41c434906bbe64b8ed7fe3c959067a9d7804
Virajith Dissanayaka <121111846+virajithdissanayaka@users.noreply.github.com>
2025-06-22 23:23:12 +0530

    aita claered

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aita/', // This is crucial for proper asset loading
 })
