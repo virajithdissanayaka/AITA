e64b7657c562a6915c5b5c62935f5437e947058a
Virajith Dissanayaka <121111846+virajithdissanayaka@users.noreply.github.com>
2025-06-22 19:54:17 +0530

    api link added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://71894a97-cdc9-4cf7-a54e-2a8cdf18fd26-00-2fv81yvrfjeuo.pike.replit.dev/'
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
