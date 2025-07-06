fd8657a2b959e32f6dbd0f7580adbb9b60a0b71e
Virajith Dissanayaka <121111846+virajithdissanayaka@users.noreply.github.com>
2025-07-06 11:53:20 +0530

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL; http://140.245.224.160:8000
-const API_BASE_URL = 'http://140.245.224.160:8000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
